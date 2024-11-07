1eab71513b6d0a5cb6ba4e8ab9f2b0ef7ba54771
Rafif Zahran Haryadi <140896342+Gregroww@users.noreply.github.com>
2024-11-07 21:11:14 +0800

    Create menu_admin.php
    add menu_admin

---
+++ menu_admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Menu</title>
+    <link rel="stylesheet" href="style/menu_admin.css">
+</head>
+<body>
+    <div class="admin-menu">
+        <h1>Menu Admin</h1>
+        <p>Pilih salah satu menu di bawah untuk melanjutkan:</p>
+        <div class="button-container">
+            <button onclick="location.href='admin_list_barang.php'">Menu Barang</button>
+            <button onclick="location.href='admin_list_toko.php'">Menu Toko</button>
+        </div>
+    </div>
+</body>
+</html>
